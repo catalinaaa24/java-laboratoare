6c38f0564352c57e177e3203d320313ee1b312b3
catalinaaa24 <georgiana.musat@student.unitbv.ro>
2023-10-25 18:40:22 +0300

    lab1

--- laborator2/src/Main.java
+++ laborator2/src/Main.java
@@ -4,14 +4,34 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        int month_days [ ] = { 31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31 } ;
+        String months [ ] = { "January" , "February" , "March" , "April" , "May" , "June" ,
+                "July" , "August" , "September" , "October" , "November" , "December" } ;
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // 1.4.1
+        for (int i = 0; i < months.length; i++) {
+            System.out.println(months[i] + " -> " + month_days[i]);
+        }
+        // 1.4.2
+        for (int i = 0; i < months.length; i++) {
+            if(month_days[i]==31)
+           System.out.println(months[i]);
+        }
+        //1.4.3
+        for (int i = 0; i < months.length; i++) {
+            if(month_days[i]<31)
+            System.out.println(months[i] + " -> " + month_days[i]);
+        }
+        //1.4.4
+        int sum=0;
+        for (int i = 0; i < months.length; i++) {
+            if(month_days[i]<31)
+                sum = sum +month_days[i];
+        }
+        System.out.println("sum="+ sum);
+        //1.4.5
+        for (int i = 0; i < months.length; i++) {
+            System.out.println(months[i].substring(0,3));
         }
     }
 }
